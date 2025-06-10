a1c9747d837ff1293df3cfb974cd304fc96613f4
Atul-ram-3152 <atulram3152@gmail.com>
2025-06-11 00:17:14 +0530

    Implement category and product management features with admin registration, slug field updates, and store views

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     'category',
     'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "category.context_processors.menu_links",
             ],
         },
     },
@@ -119,10 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR /"static"
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
+
+
+#media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
