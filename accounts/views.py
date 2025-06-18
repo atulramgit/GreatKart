16a425ca64cf4726a725ede7fbda19d5be9b8a21
Atul-ram-3152 <atulram3152@gmail.com>
2025-06-18 19:20:55 +0530

    Implement user-specific cart management and checkout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,9 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -70,6 +73,16 @@ def login(request):
         print("Authenticated User:", user)
         
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
             return redirect('dashboard')
